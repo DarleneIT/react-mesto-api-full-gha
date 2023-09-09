971bebe877927891cd397a3cad44c40ca1e6349f
Daria <daria.wt@yandex.ru>
2023-09-10 01:29:26 +0300

    15 ПР - добавлен env

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,3 +1,11 @@
-const SECRET_KEY = '0beb9865b2a6f1d2b90f66a9a8e6c642e0b8e34593c9a71f5b7cbce0cdd2e2cd';
+const { config } = require('dotenv');
+
+const { SECRET_KEY = 'secret' } = process.env;
+
+const { NODE_ENV } = process.env;
+
+if (NODE_ENV === 'production') {
+  config();
+}
 
 module.exports = { SECRET_KEY };
