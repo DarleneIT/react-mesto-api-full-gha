bc96c5090721c2bb2d1c451f89834a7f576ef8b6
Daria <daria.wt@yandex.ru>
2023-09-10 18:18:50 +0300

    15 ПР final

--- backend/app.js
+++ backend/app.js
@@ -30,7 +30,7 @@ const corsOptions = {
     'http://localhost:3000',
     'http://localhost:3001',
   ],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
